357242ca70b2aeb6426ab0d2f328520580da1fa9
Cezar Halmagean <cezar@mixandgo.com>
2016-05-25 10:42:28 +0300

    Add spec for search

--- spec/features/search_comics_spec.rb
+++ spec/features/search_comics_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature 'Search Comics', type: :feature do
-  pending "TODO"
+  it 'filters comics', js: true do
+    VCR.use_cassette('comics_list', match_requests_on: [:path]) do
+      visit '/'
+      fill_in 'search', with: 'Deadpool'
+    end
+
+    VCR.use_cassette('comics_search', match_requests_on: [:path]) do
+      click_on 'search-submit'
+      expect(page).to have_css('.comic-title', text: 'Deadpool (2008) #55', count: 1)
+    end
+  end
 end
 
