ee0c4873daa0929494ad620e163dff64692c4a00
Cezar Halmagean <cezar@mixandgo.com>
2016-05-25 08:18:18 +0300

    Update the actions
    The actions should be different depending on the state of each comic
    (upvoted or not).

--- app/helpers/comics_helper.rb
+++ app/helpers/comics_helper.rb
@@ -1,11 +1,32 @@
 module ComicsHelper
-  def comic_box(comic)
+  def comic_box(comic, votes)
     html = ""
     img_path = "#{comic.thumbnail.path}.#{comic.thumbnail.extension}"
 
     html += content_tag(:div, nil, class: 'comic-box', style: "background-image: url('#{img_path}');")
-    html += content_tag(:span, comic.title)
+    html += content_tag(:div, upvote(comic, votes), class: 'comic-actions')
+    html += content_tag(:span, comic.title, class: 'comic-title')
 
     html.html_safe
   end
+
+  private
+
+    def upvote(comic, votes)
+      if votes.include?(comic.id)
+        content_tag(:span, '', class: 'fa fa-star')
+      else
+        upvote_action(comic)
+      end
+    end
+
+    def upvote_action(comic)
+      button_tag(
+        type: 'button',
+        class: "btn btn-default upvote-btn",
+        id: "comic-upvote-btn-#{comic.id}",
+        data: { comic_id: comic.id }) do
+          content_tag(:span, '', class: 'fa fa-thumbs-o-up')
+      end
+    end
 end
--- spec/helpers/comics_helper_spec.rb
+++ spec/helpers/comics_helper_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ComicsHelper, type: :helper do
+  let(:thumb) { double('Thumbnail', path: 'image_path', extension: 'jpg') }
+  let(:comic) { double('Comic', id: 1, title: 'Awesome', thumbnail: thumb) }
+
   it 'wraps the comic in some html' do
-    thumb = double('Thumbnail', path: 'image_path', extension: 'jpg')
-    comic = double('Comic', title: 'Awesome', thumbnail: thumb)
-    output = helper.comic_box(comic)
+    output = helper.comic_box(comic, [])
     expect(output).to have_css('.comic-box', count: 1)
+    expect(output).to have_css('.comic-actions', count: 1)
+    expect(output).to have_css('.upvote-btn', count: 1)
     expect(output).to have_css('span', text: 'Awesome', count: 1)
+  end
+
+  context 'when the comic was already upvoted' do
+    it 'replaces the button with a star' do
+      output = helper.comic_box(comic, [comic.id])
+      expect(output).to have_css('.fa-star', count: 1)
+      expect(output).to_not have_css('.upvote-btn', count: 1)
+    end
   end
 end
