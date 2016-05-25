e927208979148801b65b8604d4ed1be184e54a5a
Cezar Halmagean <cezar@mixandgo.com>
2016-05-25 08:06:47 +0300

    Add feature test

--- spec/features/upvote_comic_spec.rb
+++ spec/features/upvote_comic_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature 'Upvote Comic', type: :feature do
-  pending "TODO"
+  it 'upvotes a comic', js: true do
+    VCR.use_cassette('comics_list', match_requests_on: [:path]) do
+      visit '/'
+      find('.upvote-btn', match: :first).click
+      expect(page).to have_css('.fa-star', count: 1)
+    end
+  end
 end
 
 
